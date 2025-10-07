348c8bc6e2805448f2dd44f1340c73dbbab8a665
KIRUTHIKA <kirthi2001va@gmail.com>
2025-10-07 12:00:04 +0530

    Frequency Code

---
+++ frequency.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Frequency(s string) map[string]int {
+	freq := map[string]int{}
+
+	for i := 0; i < len(s); i++ {
+		char := string(s[i])
+		freq[char]++
+	}
+
+	return freq
+}
+
+func main() {
+	result := Frequency("kiruthika")
+	fmt.Println(result)
+
+}
